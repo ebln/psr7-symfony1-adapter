42d431df5058ad4536d1b8281f008446326f4b1b
ebln <34722048+ebln@users.noreply.github.com>
2018-11-25 12:23:21 +0100

    outsource a common head trait

---
+++ src/Implementation/Hidden/CommonHeadTrait.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace brnc\Symfony1\Message\Implementation\Hidden;
+
+trait CommonHeadTrait
+{
+    /** @var string */
+    protected $protocolVersion = '';
+
+    /** @var string[][]|string[][][] */
+    protected $headers = [];
+
+    /** @var string[] */
+    protected $headersRemoved = [];
+
+    /** @var string[] */
+    protected $headersReplaced = [];
+
+    /**
+     * @param string       $version
+     * @param string[][][] $headers
+     */
+    public function __construct($version, array $headers)
+    {
+        $this->protocolVersion = $version;
+        $this->headers         = $headers;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProtocolVersion()
+    {
+        return $this->protocolVersion;
+    }
+
+    /**
+     * @param string $version
+     *
+     * @return static
+     */
+    public function withProtocolVersion($version)
+    {
+        $clone                  = clone $this;
+        $clone->protocolVersion = $version;
+
+        return $clone;
+    }
+
+    /**
+     * @return array|\string[][]
+     */
+    public function getHeaders()
+    {
+        return array_column($this->headers, Constant::HEADER_CONTENT, Constant::HEADER_NAME);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasHeader($name)
+    {
+        return isset($this->headers[strtolower($name)][Constant::HEADER_CONTENT]);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return array|string[]
+     */
+    public function getHeader($name)
+    {
+        $normalisedName = strtolower($name);
+
+        return isset($this->headers[$normalisedName][Constant::HEADER_CONTENT])? $this->headers[$normalisedName][Constant::HEADER_CONTENT] : [];
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    public function getHeaderLine($name)
+    {
+        return implode(', ', $this->getHeader($name));
+    }
+
+    /**
+     * @param string          $name
+     * @param string|string[] $value
+     *
+     * @return static
+     */
+    public function withHeader($name, $value)
+    {
+        $clone     = clone $this;
+        $fieldName = strtolower($name);
+
+        $clone->headers[$fieldName] = [
+            Constant::HEADER_NAME    => $name,
+            Constant::HEADER_CONTENT => is_array($value)? $value : [$value],
+        ];
+        unset($clone->headersRemoved[$fieldName]);
+        $clone->headersReplaced[$fieldName] = true;
+
+        return $clone;
+    }
+
+    /**
+     * @param string          $name
+     * @param string|string[] $value
+     *
+     * @return static
+     */
+    public function withAddedHeader($name, $value)
+    {
+        $clone     = clone $this;
+        $fieldName = strtolower($name);
+        $values    = is_array($value)? $value : [$value];
+
+        if (isset($clone->headers[$fieldName])) {
+            $clone->headers[$fieldName][Constant::HEADER_CONTENT] = array_merge($clone->headers[$fieldName][Constant::HEADER_CONTENT],
+                                                                            $values
+            );
+        }
+        else {
+            $clone->headers[$fieldName] = [
+                Constant::HEADER_NAME    => $name,
+                Constant::HEADER_CONTENT => $values,
+            ];
+            unset($clone->headersRemoved[$fieldName]);
+            $clone->headersReplaced[$fieldName] = true;
+        }
+
+        return $clone;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return static
+     */
+    public function withoutHeader($name)
+    {
+        $fieldName = strtolower($name);
+
+        if (!isset($this->headers[$fieldName])) {
+            return $this;
+        }
+
+        $clone = clone $this;
+        unset($clone->headers[$fieldName], $clone->headersReplaced[$fieldName]);
+        $clone->headersRemoved[$fieldName] = true;
+
+        return $clone;
+    }
+}
---
+++ src/Implementation/Hidden/Constant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace brnc\Symfony1\Message\Implementation\Hidden;
+
+
+class Constant
+{
+    CONST HEADER_NAME    = 'name';
+    CONST HEADER_CONTENT = 'values';
+}
