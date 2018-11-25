b325dd7c9aebd9d07329d171acc3bd43c2ce180c
ebln <34722048+ebln@users.noreply.github.com>
2018-11-25 13:47:13 +0100

    change doc : remove copied phpdoc from interface

--- src/Implementation/SubsetServerRequest.php
+++ src/Implementation/SubsetServerRequest.php
@@ -35,7 +35,7 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * @inheritdoc
+     * @return array
      */
     public function getServerParams()
     {
@@ -43,7 +43,7 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * @inheritdoc
+     * @return array
      */
     public function getCookieParams()
     {
@@ -51,7 +51,9 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * @inheritdoc
+     * @param array $cookies
+     *
+     * @return static
      */
     public function withCookieParams(array $cookies)
     {
@@ -62,15 +64,6 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * Retrieve query string arguments.
-     *
-     * Retrieves the deserialized query string arguments, if any.
-     *
-     * Note: the query params might not be in sync with the URI or server
-     * params. If you need to ensure you are only getting the original
-     * values, you may need to parse the query string from `getUri()->getQuery()`
-     * or from the `QUERY_STRING` server param.
-     *
      * @return array
      */
     public function getQueryParams()
@@ -79,25 +72,7 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * Return an instance with the specified query string arguments.
-     *
-     * These values SHOULD remain immutable over the course of the incoming
-     * request. They MAY be injected during instantiation, such as from PHP's
-     * $_GET superglobal, or MAY be derived from some other value such as the
-     * URI. In cases where the arguments are parsed from the URI, the data
-     * MUST be compatible with what PHP's parse_str() would return for
-     * purposes of how duplicate query parameters are handled, and how nested
-     * sets are handled.
-     *
-     * Setting query string arguments MUST NOT change the URI stored by the
-     * request, nor the values in the server params.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * updated query string arguments.
-     *
-     * @param array $query Array of query string arguments, typically from
-     *                     $_GET.
+     * @param array $query
      *
      * @return static
      */
@@ -110,19 +85,7 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * Retrieve any parameters provided in the request body.
-     *
-     * If the request Content-Type is either application/x-www-form-urlencoded
-     * or multipart/form-data, and the request method is POST, this method MUST
-     * return the contents of $_POST.
-     *
-     * Otherwise, this method may return any results of deserializing
-     * the request body content; as parsing returns structured content, the
-     * potential types MUST be arrays or objects only. A null value indicates
-     * the absence of body content.
-     *
-     * @return null|array|object The deserialized body parameters, if any.
-     *     These will typically be an array or object.
+     * @return array|object|null
      */
     public function getParsedBody()
     {
@@ -130,33 +93,9 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * Return an instance with the specified body parameters.
-     *
-     * These MAY be injected during instantiation.
-     *
-     * If the request Content-Type is either application/x-www-form-urlencoded
-     * or multipart/form-data, and the request method is POST, use this method
-     * ONLY to inject the contents of $_POST.
-     *
-     * The data IS NOT REQUIRED to come from $_POST, but MUST be the results of
-     * deserializing the request body content. Deserialization/parsing returns
-     * structured data, and, as such, this method ONLY accepts arrays or objects,
-     * or a null value if nothing was available to parse.
-     *
-     * As an example, if content negotiation determines that the request data
-     * is a JSON payload, this method could be used to create a request
-     * instance with the deserialized parameters.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * updated body parameters.
-     *
-     * @param null|array|object $data The deserialized body data. This will
-     *                                typically be in an array or object.
+     * @param $data
      *
      * @return static
-     * @throws \InvalidArgumentException if an unsupported argument type is
-     *     provided.
      */
     public function withParsedBody($data)
     {
@@ -167,15 +106,7 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * Retrieve attributes derived from the request.
-     *
-     * The request "attributes" may be used to allow injection of any
-     * parameters derived from the request: e.g., the results of path
-     * match operations; the results of decrypting cookies; the results of
-     * deserializing non-form-encoded message bodies; etc. Attributes
-     * will be application and request specific, and CAN be mutable.
-     *
-     * @return array Attributes derived from the request.
+     * @return array
      */
     public function getAttributes()
     {
@@ -183,19 +114,8 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * Retrieve a single derived request attribute.
-     *
-     * Retrieves a single derived request attribute as described in
-     * getAttributes(). If the attribute has not been previously set, returns
-     * the default value as provided.
-     *
-     * This method obviates the need for a hasAttribute() method, as it allows
-     * specifying a default value to return if the attribute is not found.
-     *
-     * @see getAttributes()
-     *
-     * @param string $name    The attribute name.
-     * @param mixed  $default Default value to return if the attribute does not exist.
+     * @param string $name
+     * @param mixed  $default
      *
      * @return mixed
      */
@@ -209,19 +129,8 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * Return an instance with the specified derived request attribute.
-     *
-     * This method allows setting a single derived request attribute as
-     * described in getAttributes().
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * updated attribute.
-     *
-     * @see getAttributes()
-     *
-     * @param string $name  The attribute name.
-     * @param mixed  $value The value of the attribute.
+     * @param $name
+     * @param $value
      *
      * @return static
      */
@@ -234,18 +143,7 @@ class SubsetServerRequest extends SubsetRequest
     }
 
     /**
-     * Return an instance that removes the specified derived request attribute.
-     *
-     * This method allows removing a single derived request attribute as
-     * described in getAttributes().
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that removes
-     * the attribute.
-     *
-     * @see getAttributes()
-     *
-     * @param string $name The attribute name.
+     * @param $name
      *
      * @return static
      */
